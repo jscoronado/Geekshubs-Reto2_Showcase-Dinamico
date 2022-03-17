08825b0ca3d805a2a4a851365efdc83c58d9ea00
Jose <jcoronadoar@gmail.com>
2022-03-17 01:47:14 +0100

    08. Añadido eventListenere para manejar la disposición de la caja del carrito donde unicamente se podrá arrastrar el producto.

--- js/dist/cart.js
+++ js/dist/cart.js
@@ -289,5 +289,17 @@ window.onload = (e) => {
 	displayCartProducts();
 }
 
+/* # Add event to drag and drop to cart
+-------------------------------------------------------------- */
+function addEvent(element, event, delegate ) {
+	if (typeof (window.event) != 'undefined' && element.attachEvent)
+		element.attachEvent('on' + event, delegate);
+	else
+		element.addEventListener(event, delegate, false);
+}
 
+addEvent(document, 'readystatechange', function() {
+	addEvent(cart_product, 'drop', onDrop);
+	addEvent(cart_product, 'dragover', onDragOver);
+});
 
